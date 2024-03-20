09f1e52e46345efdef72d1ef788480103d5b03f8
nathaniel nosa <nathanielnarth@gmail.com>
2024-03-20 10:09:56 +0100

    login controller changes

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken')
 const CryptoJs = require('crypto-js')
 
 const handleLogin = async (req, res) => {
-  const { username, password } = req.body
+  const { username, password, rememberMe } = req.body
   if (!username || !password) return res.status(401).json("Enter username/password")
   try {
     const logUser = await User.findOne({ username }).exec()
@@ -14,17 +14,27 @@ const handleLogin = async (req, res) => {
     if (match !== password) return res.status(401).json("Invalid username/password")
 
     //jwt
+    // Generate JWT token
+    const expiresIn = rememberMe ? '7d' : '1d';
     const access_token = jwt.sign(
       { id: logUser._id, isAdmin: logUser.isAdmin },
       process.env.JWT,
-      { expiresIn: '3d' }
+      { expiresIn }
     )
+    // Set the JWT token in a cookie with httpOnly flag
+    res.cookie('token', access_token, {
+      httpOnly: true,
+      sameSite: 'strict',
+      expires: rememberMe ? new Date(Date.now() + 7 * 24 * 60 * 60 * 1000) : undefined
+      // secure: true,
+    });
+
 
     const { password: userPwd, ...others } = logUser._doc
-    res.status(200).json(`${others.firstname} logged in successfully,${access_token}`)
+    res.status(200).json({ message: `${others.firstname} logged in successfully`, access_token });
 
   } catch (error) {
-    res.status(500).json(`Error: ${error.message}`)
+    res.status(500).json({ error: error.message });
   }
 }
 
